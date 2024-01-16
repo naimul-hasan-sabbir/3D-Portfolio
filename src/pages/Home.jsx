f52efb4983ea48adef1772df37035be2727f72c2
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2024-01-16 11:15:42 +0600

    Loading screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import {Suspense} from 'react'
 import { Canvas } from '@react-three/fiber'
+import Loader from '../component/Loader'
 
 const Home = () => {
   return (
@@ -11,6 +12,9 @@ const Home = () => {
       className='w-full h-screen bg-transparent'
       camera={{ near: 0.1, far: 1000 }}
       >
+        <Suspense fallback={<Loader />}>
+
+        </Suspense>
       </Canvas>
     </section>
   )
