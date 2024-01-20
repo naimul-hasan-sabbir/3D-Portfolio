ac97e369d9236158d0a0db0e2f81aae460307b6d
naimul-hasan-sabbir <naimul.hasan.sabbir@g.bracu.ac.bd>
2024-01-20 13:54:36 +0600

    modified island jsx

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -13,55 +13,40 @@ import islandScene from '../assets/3d/island.glb';
 import {a} from '@react-spring/three';
 
 const Island = (props) => {
+  const islandRef = useRef();
   const { nodes, materials } = useGLTF("/island.glb");
   return (
-    <a.group {...props} dispose={null}>
+    <a.group ref={islandRef} {...props} dispose={null}>
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface944_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface945_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface946_tree2_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface947_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface948_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface949_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.pCube11_rocks1_0.geometry}
         material={materials.PaletteMaterial001}
       />
-    </group>
+    </a.group>
   );
 }
-
-useGLTF.preload("/island.glb");
 
 export default Island;
